d3505a1cde66add3146e286e156117251a15e4ed
ani2340 <121184004+ani2340@users.noreply.github.com>
2023-02-24 03:15:29 +0000

    2/22 and 2/23 work

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import math
 class Vector2d:
     def __init__(self, x, y):
         self.x = x
@@ -8,3 +9,16 @@ class Vector2d:
         return f"{self.x}i + {self.y}j"
     def __abs__(self):
         return ((self.x**2) + (self.y**2))**(0.5)
+    def __neg__(self):
+        return Vector2d(self.x*-1, self.y*-1)
+    def __add__(self, other):
+        return Vector2d((self.x + other.x),(self.y + other.y))
+    def __eq__(self, other):
+        if (self.x == self.y) == (other.x, other.y):
+            return True
+        return False
+    def __sub__(self, other):
+        return Vector2d((self.x - other.x), (self.y - other.y))
+    def angle(self, other):
+        tan = math.atan2((self.y), (self.x))
+        return math.degrees(tan)
